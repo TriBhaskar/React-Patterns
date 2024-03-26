7db4d701924b156063fa2b74bae17595c84da7b7
TriBhaskar <panthribhaskar@gmail.com>
2024-03-26 22:23:26 +0530

    Add Accordion component to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,38 @@
+import Accordion from "./components/Accordion/Accordion";
+import AccordionItem from "./components/Accordion/AccordionItem";
+
 function App() {
-  return <h1>React Patterns & Practices</h1>;
+  return (
+    <main>
+      <section>
+        <h2>Why work with us?</h2>
+        <Accordion className="accordion">
+          <AccordionItem title="We got 20 years of experience">
+            <article>
+              <p>You can&apos;t go wrong with us</p>
+              <p>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Earum
+                quo architecto quia facere, laboriosam id perferendis cumque
+                voluptatibus ipsam et, ratione harum dignissimos eveniet libero
+                eius quisquam quibusdam dolore repellendus.
+              </p>
+            </article>
+          </AccordionItem>
+          <AccordionItem title="We're working with local guides">
+            <article>
+              <p>We are not doing this along from our office.</p>
+              <p>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Earum
+                quo architecto quia facere, laboriosam id perferendis cumque
+                voluptatibus ipsam et, ratione harum dignissimos eveniet libero
+                eius quisquam quibusdam dolore repellendus.
+              </p>
+            </article>
+          </AccordionItem>
+        </Accordion>
+      </section>
+    </main>
+  );
 }
 
 export default App;
