18e00bd33e40d84c86590e4e51e0e91c596632e0
TriBhaskar <panthribhaskar@gmail.com>
2024-03-28 23:54:10 +0530

    Refactor AccordionItem component to use arrow function for onClick event handler

--- src/components/Accordion/AccordionItem.jsx
+++ src/components/Accordion/AccordionItem.jsx
@@ -3,12 +3,9 @@ import { useAccordionContext } from "./Accordion";
 export default function AccordionItem({ id, className, title, children }) {
   const { openItemId, toggleItem } = useAccordionContext();
   const isOpen = openItemId === id;
-  function handleClick() {
-    toggleItem(id);
-  }
   return (
     <li className={className}>
-      <h3 onClick={handleClick}>{title}</h3>
+      <h3 onClick={() => toggleItem(id)}>{title}</h3>
       <div
         className={
           isOpen ? "accordion-item-content open" : "accordion-item-content"
