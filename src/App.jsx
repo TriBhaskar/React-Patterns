ede42c7c938c13db10de0c8450c385eeb31749b5
TriBhaskar <panthribhaskar@gmail.com>
2024-03-30 23:14:21 +0530

    Refactor Accordion component and update styles

--- src/App.jsx
+++ src/App.jsx
@@ -7,35 +7,43 @@ function App() {
       <section>
         <h2>Why work with us?</h2>
         <Accordion className="accordion">
-          <Accordion.Item
-            id="experience"
-            className="accordion-item"
-            title="We got 20 years of experience"
-          >
-            <article>
-              <p>You can&apos;t go wrong with us</p>
-              <p>
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Earum
-                quo architecto quia facere, laboriosam id perferendis cumque
-                voluptatibus ipsam et, ratione harum dignissimos eveniet libero
-                eius quisquam quibusdam dolore repellendus.
-              </p>
-            </article>
+          <Accordion.Item id="experience" className="accordion-item">
+            <Accordion.Title id="experience" className="accordion-item-title">
+              We got 20 years of experience
+            </Accordion.Title>
+            <Accordion.Content
+              id="experience"
+              className="accordion-item-content"
+            >
+              <article>
+                <p>You can&apos;t go wrong with us</p>
+                <p>
+                  Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                  Earum quo architecto quia facere, laboriosam id perferendis
+                  cumque voluptatibus ipsam et, ratione harum dignissimos
+                  eveniet libero eius quisquam quibusdam dolore repellendus.
+                </p>
+              </article>
+            </Accordion.Content>
           </Accordion.Item>
-          <Accordion.Item
-            id="local-guides"
-            className="accordion-item"
-            title="We're working with local guides"
-          >
-            <article>
-              <p>We are not doing this along from our office.</p>
-              <p>
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Earum
-                quo architecto quia facere, laboriosam id perferendis cumque
-                voluptatibus ipsam et, ratione harum dignissimos eveniet libero
-                eius quisquam quibusdam dolore repellendus.
-              </p>
-            </article>
+          <Accordion.Item id="local-guides" className="accordion-item">
+            <Accordion.Title id="local-guides" className="accordion-item-title">
+              We're working with local guides
+            </Accordion.Title>
+            <Accordion.Content
+              id="local-guides"
+              className="accordion-item-content"
+            >
+              <article>
+                <p>We are not doing this along from our office.</p>
+                <p>
+                  Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                  Earum quo architecto quia facere, laboriosam id perferendis
+                  cumque voluptatibus ipsam et, ratione harum dignissimos
+                  eveniet libero eius quisquam quibusdam dolore repellendus.
+                </p>
+              </article>
+            </Accordion.Content>
           </Accordion.Item>
         </Accordion>
       </section>
