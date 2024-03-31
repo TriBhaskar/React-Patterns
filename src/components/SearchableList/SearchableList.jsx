b72bcc7d90967bb7ae636ee83dde9ff4556afd94
TriBhaskar <panthribhaskar@gmail.com>
2024-03-31 16:32:15 +0530

    Add debounce functionality to search input in SearchableList component

--- src/components/SearchableList/SearchableList.jsx
+++ src/components/SearchableList/SearchableList.jsx
@@ -1,12 +1,20 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 export default function SearchableList({ items, itemKeyFn, children }) {
+  const lastChange = useRef();
   const [searchTerm, setSeachTerm] = useState("");
   const searchResults = items.filter((item) =>
     JSON.stringify(item).toLowerCase().includes(searchTerm.toLowerCase())
   );
   function handleChange(event) {
-    setSeachTerm(event.target.value);
+    // implementing debounce which will wait for 500ms before updating the search term helps to reduce the number of times the search results are updated
+    if (lastChange.current) {
+      clearTimeout(lastChange.current);
+    }
+    lastChange.current = setTimeout(() => {
+      lastChange.current = null;
+      setSeachTerm(event.target.value);
+    }, 500);
   }
   return (
     <div className="searchable-list">
